8e89e233e306512a2547a21ba151c29d0eff6f86
Minh Triet <bindsu63@gmail.com>
2022-04-11 00:11:05 +0700

    restructure

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const db = require('./config/db');
 
 db.connect();
-const port = 4000;
+const port = 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${process.env.PORT || port}...`);
 });
