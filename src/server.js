a7e3ad90938aad0d44e6ddc6b030773c0aae99c9
Minh Triet <bindsu63@gmail.com>
2022-04-13 20:08:41 +0700

    rm env / node_modules

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const db = require('./config/db');
 
 db.connect();
-const port = 5000;
+const port = 4000;
 app.listen(port, () => {
     console.log(`Server running on port ${process.env.PORT || port}...`);
 });
