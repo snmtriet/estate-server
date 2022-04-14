ed9a93b5e345d3c6f2a1e015b5cc58144149ceb9
Minh Triet <bindsu63@gmail.com>
2022-04-14 12:01:55 +0700

    port heroku change

--- src/server.js
+++ src/server.js
@@ -3,7 +3,4 @@ const app = require('./app');
 const db = require('./config/db');
 
 db.connect();
-const port = 4000;
-app.listen(port, () => {
-    console.log(`Server running on port ${process.env.PORT || port}...`);
-});
+app.listen(process.env.PORT || 4000);
